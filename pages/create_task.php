fef595c79d05f5fedff3996d3437477e69cf043b
Juan Zabala <juanzabaladev@gmail.com>
2021-06-05 02:02:37 -0500

    fix redirect from create task

--- pages/create_task.php
+++ pages/create_task.php
@@ -11,7 +11,7 @@ if (!empty($task) || !empty($description) || !empty($date)) {
     // database connection
     $taskModel = new TaskModel();
     $taskModel->insert($task, $description, $date);
-    $pageToRedirect = "/web1_g1_test/pages/list_tasks.php";
+    $pageToRedirect = "/pages/list_tasks.php";
     header("Location: {$pageToRedirect}");
     exit;
 } else {
